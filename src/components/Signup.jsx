26d69abdabf83d5189b2f9c965cdc8fc6bc84990
Raushan Sharma01~ <raushankumar7485852533@gmail.com>
2023-07-15 15:51:44 +0530

    changes

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
-import {  useNavigate } from 'react-router-dom';
-import {  createUserWithEmailAndPassword  } from 'firebase/auth';
+ import {  createUserWithEmailAndPassword  } from 'firebase/auth';
 import { auth } from '../firebase';
 import { Link} from 'react-router-dom';
 
